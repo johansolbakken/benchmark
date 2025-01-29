fae86d79e30f82cf1149b2d633e6fa223f4282e8
Johan Solbakken <johan.hvaler@gmail.com>
2025-01-29 10:05:28 +0100

    Add experimental setup sql file

---
+++ sql/experimental_setup.sql
@@ -0,0 +1,34 @@
+-- Obtained from: "Using Re-Optimization in MySQL to Improve Query Plans"
+-- NTNU Master Thesis, 2024 (Elton & Rosendahl) - https://hdl.handle.net/11250/3153611
+
+UPDATE mysql.engine_cost SET cost_value = 0.25 WHERE cost_name = 'io_block_read_cost';
+UPDATE mysql.engine_cost SET cost_value = 0.25 WHERE cost_name = 'memory_block_read_cost';
+
+FLUSH OPTIMIZER_COSTS;
+
+SET PERSIST innodb_stats_persistent = 1;
+SET PERSIST innodb_stats_auto_recalc = 0;
+
+SET GLOBAL optimizer_switch = 'hypergraph_optimizer=on';
+
+ANALYZE TABLE aka_name;
+ANALYZE TABLE aka_title;
+ANALYZE TABLE cast_info;
+ANALYZE TABLE char_name;
+ANALYZE TABLE company_name;
+ANALYZE TABLE company_type;
+ANALYZE TABLE comp_cast_type;
+ANALYZE TABLE complete_cast;
+ANALYZE TABLE info_type;
+ANALYZE TABLE keyword;
+ANALYZE TABLE kind_type;
+ANALYZE TABLE link_type;
+ANALYZE TABLE movie_companies;
+ANALYZE TABLE movie_info;
+ANALYZE TABLE movie_info_idx;
+ANALYZE TABLE movie_keyword;
+ANALYZE TABLE movie_link;
+ANALYZE TABLE name;
+ANALYZE TABLE person_info;
+ANALYZE TABLE role_type;
+ANALYZE TABLE title;
