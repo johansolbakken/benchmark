5a420f0e77d7981ace9a59118b941445f1ed7f53
Morten Sunde <mtrsunde@outlook.com>
2025-02-25 16:19:59 +0100

    Benchmark complete

--- bin/hyperfine.rb
+++ bin/hyperfine.rb
@@ -151,7 +151,7 @@ def run_baseline(queries, warmup_runs, total_runs)
   puts "------------------------------------"
 end
 
-OPTIMISM_VALUES    = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+OPTIMISM_VALUES    = [0.4,0.5,0.6,0.7,0.8,0.9,1.0]
 OPTIMISM_FUNCTIONS = [
   #'NONE', 
   'LINEAR', 
