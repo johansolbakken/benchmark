e95db6a321c1c4ebdb720c0ded158746ed19b3ea
Johan Solbakken <johan.hvaler@gmail.com>
2025-02-27 14:51:01 +0100

    Set join buffer size

--- sql/experimental_setup.sql
+++ sql/experimental_setup.sql
@@ -1,4 +1,4 @@
--- Obtained from: 'Using Re-Optimization in MySQL to Improve Query Plans'
+-- Based on: 'Using Re-Optimization in MySQL to Improve Query Plans'
 -- NTNU Master Thesis, 2024 (Elton & Rosendahl) - https://hdl.handle.net/11250/3153611
 
 UPDATE mysql.engine_cost SET cost_value = 0.25 WHERE cost_name = 'io_block_read_cost';
@@ -8,5 +8,7 @@ FLUSH OPTIMIZER_COSTS;
 
 SET PERSIST innodb_stats_persistent = 1;
 SET PERSIST innodb_stats_auto_recalc = 0;
+SET PERSIST innodb_buffer_pool_size = 17179869184; -- 16GB
+SET PERSIST join_buffer_size = 1048576; -- 1MB
 
 SET GLOBAL optimizer_switch = 'hypergraph_optimizer=on';
