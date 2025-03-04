c15b2d4f8ee3088d836981717b5570b96c659ec1
Johan Solbakken <johan.hvaler@gmail.com>
2025-03-04 15:57:31 +0100

    Add DOT export feature for query plans

--- bin/print-plan-as-graphwiz.rb
+++ bin/print-plan-as-graphwiz.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
 require 'optparse'
 require 'yaml'
-require 'terminal-table'
-require 'csv'
 require 'json'
 require 'fileutils'
+
 require_relative '../lib/mysql'
 
 # Load MySQL configuration and create a client.
 CONFIG = YAML.load_file('config.yaml')['mysql']
-CLIENT = MySQL::Client.new(CONFIG['user'], CONFIG['port'], CONFIG['host'], CONFIG['name'], CONFIG['path'])
+CLIENT = MySQL::Client.new(CONFIG['user'], CONFIG['port'], CONFIG['host'], CONFIG['name'], CONFIG['path'], true)
 
 # Convert a JSON query plan to DOT graph format.
 def json_to_dot(data)
@@ -46,24 +45,54 @@ def json_to_dot(data)
   dot_lines.join("\n")
 end
 
-def run
-  # Read the SQL query from file and prepare the EXPLAIN statement.
-  query = File.read('./job-order-queries/10a.sql').lines.map(&:strip).join(' ')
+def run(input_sql_file, show_json, output_png, keep_dot)
+  query = File.read(input_sql_file).lines.map(&:strip).join(' ')
   explain_query = "EXPLAIN FORMAT=json #{query}"
   stdout, _stderr = CLIENT.run_query_get_stdout(explain_query)
   text_without_newlines = stdout.gsub(/\\n/, "")
   json_text = text_without_newlines.delete_prefix("EXPLAIN\n")
   data = JSON.parse(json_text)
 
-  puts JSON.pretty_generate(data)
+  puts JSON.pretty_generate(data) if show_json
 
-  # Generate and save the DOT graph.
   dot_output = json_to_dot(data)
-  File.write('query_plan.dot', dot_output)
-  puts 'DOT file has been written to query_plan.dot'
+  dot_file = 'query_plan.dot'
+  File.write(dot_file, dot_output)
+  puts "DOT file has been written to #{dot_file}"
 
-  system('dot -Tpng -Gdpi=600 -o query_plan.png query_plan.dot')
+  system("dot -Tpng -Gdpi=600 -o #{output_png} #{dot_file}")
+  puts "PNG file has been written to #{output_png}"
+
+  File.delete(dot_file) unless keep_dot
   puts 'Successful success!'
 end
 
-run if __FILE__ == $PROGRAM_NAME
+options = {
+  show_json: false,
+  output_png: 'query_tree.png',
+  keep_dot: false
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename($PROGRAM_NAME)} [options] input_sql_file"
+
+  opts.on("--show-json", "Print JSON output (default false)") do
+    options[:show_json] = true
+  end
+
+  opts.on("-oOUTPUT", "--output=OUTPUT", "Output PNG file name (default query_tree.png)") do |output|
+    options[:output_png] = output
+  end
+
+  opts.on("-c", "--keep-dot", "Keep the DOT file (default delete it)") do
+    options[:keep_dot] = true
+  end
+end.parse!
+
+if ARGV.empty?
+  puts "Error: input SQL file is required."
+  exit 1
+end
+
+input_sql_file = ARGV[0]
+run(input_sql_file, options[:show_json], options[:output_png], options[:keep_dot]) if __FILE__ == $PROGRAM_NAME
