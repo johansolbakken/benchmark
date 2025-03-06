f0dd786b539ade9c662d0320159f3169ff5376e6
Johan Solbakken <johan.hvaler@gmail.com>
2025-03-06 12:17:22 +0100

    Generate where clause aswell

--- bin/job-generate.rb
+++ bin/job-generate.rb
@@ -7,7 +7,7 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: #{$PROGRAM_NAME} [options]"
 
-  opts.on("-oDIR", "--output=DIR", "Output directory to write join query file") do |dir|
+  opts.on("-oDIR", "--output=DIR", "Output directory to write query files") do |dir|
     options[:output_dir] = dir
   end
 end.parse!
@@ -19,27 +19,30 @@ end
 # Create the output directory if it doesn't exist
 FileUtils.mkdir_p(options[:output_dir])
 
+# Settings: JOIN_COUNT is the maximum number of joins; ORDER_BY clause appended at the end.
 JOIN_COUNT = 15
 ORDER_BY = 'ORDER BY cast_info.id;'
+# Array of where clause counts to generate.
+where_counts = [0, 1, 2, 4, 8]
 
-# Total order of joinable tables with "title" first
+# Total order of joinable tables with "title" first.
 total_order = [
   "title",          # 1. Central movie table
-  "cast_info",      # 3. Joins to title on movie_id
-  "name",           # 17. Joins from cast_info (or complete_cast) via person_id
-  "role_type",      # 11. Joins from cast_info via cast_info.role_id = role_type.id
-  "movie_info",     # 4. Joins to title on movie_id
+  "cast_info",      # 2. Joins to title on movie_id
+  "name",           # 3. Joins via cast_info.person_id
+  "role_type",      # 4. Joins via cast_info.role_id
+  "movie_info",     # 5. Joins to title on movie_id
   "movie_keyword",  # 6. Joins to title on movie_id
   "movie_link",     # 7. Joins to title on movie_id
   "movie_companies",# 8. Joins to title on movie_id
   "complete_cast",  # 9. Joins to title on movie_id
-  "kind_type",      # 10. Joins from title via title.kind_id = kind_type.id
-  "info_type",      # 12. Joins from movie_info (or movie_info_idx) via info_type_id
-  "keyword",        # 13. Joins from movie_keyword via movie_keyword.keyword_id = keyword.id
-  "link_type",      # 14. Joins from movie_link via movie_link.link_type_id = link_type.id
-  "company_name",   # 15. Joins from movie_companies via movie_companies.company_id = company_name.id
-  "company_type",   # 16. Joins from movie_companies via movie_companies.company_type_id = company_type.id
-  "person_info"     # 19. Joins to name via name.id = person_info.person_id
+  "kind_type",      # 10. Joins from title via title.kind_id
+  "info_type",      # 11. Joins from movie_info via info_type_id
+  "keyword",        # 12. Joins via movie_keyword (lookup)
+  "link_type",      # 13. Joins via movie_link (lookup)
+  "company_name",   # 14. Joins from movie_companies via company_id
+  "company_type",   # 15. Joins from movie_companies via company_type_id
+  "person_info"     # 16. Joins via name (person_info)
 ]
 
 # Map of join conditions (parent -> { child -> condition })
@@ -81,34 +84,64 @@ join_conditions = {
   }
 }
 
+# Map of example where conditions for each table, using non-id fields.
+table_conditions = {
+  "title"           => "title.title = 'the matrix'",
+  "cast_info"       => "cast_info.note LIKE '%hero%'",
+  "name"            => "name.name LIKE 'Keanu%'",
+  "role_type"       => "role_type.role = 'Lead'",
+  "movie_info"      => "movie_info.info LIKE '%Director%'",
+  "movie_keyword"   => "movie_keyword.id > 10",  # invented column
+  "movie_link"      => "movie_link.id > 10",        # invented column
+  "movie_companies" => "movie_companies.note LIKE '%studio%'",
+  "complete_cast"   => "complete_cast.status_code = 'active'",   # invented column
+  "kind_type"       => "kind_type.kind = 'Feature'",
+  "info_type"       => "info_type.info = 'Plot'",
+  "keyword"         => "keyword.keyword = 'action'",
+  "link_type"       => "link_type.link = 'sequel'",
+  "company_name"    => "company_name.name LIKE '%Warner%'",
+  "company_type"    => "company_type.kind = 'Production'",
+  "person_info"     => "person_info.info LIKE '%bio%'"
+}
+
+# For each join count (from 2 up to JOIN_COUNT), generate a base join query
 for i in 2..JOIN_COUNT
   table_count = i + 1
-
-  # Select the first TABLE_COUNT tables from total_order
+  # Select the first 'table_count' tables from total_order.
   tables_to_join = total_order[0...table_count]
 
   # Build the join query based on tables_to_join and join_conditions.
-  # For each table (after the first) we find a parent in the already-joined tables that provides a join condition.
-  join_query = "SELECT * FROM #{tables_to_join.first}"
+  base_query = "SELECT * FROM #{tables_to_join.first}"
   joined_tables = [tables_to_join.first]
 
   tables_to_join[1..-1].each do |tbl|
     parent = joined_tables.find { |p| join_conditions[p] && join_conditions[p].key?(tbl) }
     if parent
       condition = join_conditions[parent][tbl]
-      join_query += " JOIN #{tbl} ON #{condition}"
+      base_query += " JOIN #{tbl} ON #{condition}"
       joined_tables << tbl
     else
       abort "No join condition found for table #{tbl} with any of the joined tables: #{joined_tables.join(', ')}"
     end
   end
 
-  join_query += " #{ORDER_BY}"
+  # Generate a list of WHERE conditions for the selected tables using the table_conditions mapping.
+  # (Each condition is of the form "table.col = value" where col is not an id.)
+  where_conditions = tables_to_join.map { |t| table_conditions[t] }
 
-  # Write the join query to a file in the output directory
-  output_file = File.join(options[:output_dir], "q_J#{i}.sql")
-  File.write(output_file, join_query)
+  # For each desired number of WHERE conditions, if there are enough conditions, generate a query version.
+  where_counts.each do |wc|
+    next if wc > where_conditions.length  # Skip if not enough tables.
 
-  puts "Join query written to #{output_file}"
+    query = base_query.dup
+    if wc > 0
+      query += " WHERE " + where_conditions[0...wc].join(" AND ")
+    end
+    query += " #{ORDER_BY}"
 
+    # Write the query to a file with naming convention: q_J#{i}_A#{wc}.sql
+    output_file = File.join(options[:output_dir], "QJ#{i}A#{wc}.sql")
+    File.write(output_file, query)
+    puts "Query written to #{output_file}"
+  end
 end
