7dfd6fc036f15e7a8e84dbc285593e6e4f859700
Johan Solbakken <johan.hvaler@gmail.com>
2025-04-01 12:17:13 +0200

    q4

--- oohj-bench-queries/q4.sql
+++ oohj-bench-queries/q4.sql
@@ -1,51 +1,26 @@
 SELECT *
 FROM title t
-  LEFT OUTER JOIN kind_type kt
-    ON kt.id = t.kind_id
-  LEFT OUTER JOIN aka_title at
-    ON at.movie_id = t.id
-  LEFT OUTER JOIN movie_info mi
-    ON mi.movie_id = t.id
-  LEFT OUTER JOIN info_type mit
-    ON mit.id = mi.info_type_id
-  LEFT OUTER JOIN movie_info_idx mii
-    ON mii.movie_id = t.id
-  LEFT OUTER JOIN info_type miit
-    ON miit.id = mii.info_type_id
-  LEFT OUTER JOIN movie_keyword mk
-    ON mk.movie_id = t.id
-  LEFT OUTER JOIN keyword k
-    ON k.id = mk.keyword_id
-  LEFT OUTER JOIN movie_link ml
-    ON ml.movie_id = t.id
-  LEFT OUTER JOIN link_type lt
-    ON lt.id = ml.link_type_id
-  LEFT OUTER JOIN complete_cast cc
-    ON cc.movie_id = t.id
-  LEFT OUTER JOIN cast_info ci
-    ON ci.movie_id = t.id
-  LEFT OUTER JOIN role_type rt
-    ON rt.id = ci.role_id
-  LEFT OUTER JOIN comp_cast_type cct
-    ON cct.id = ci.person_role_id
-  LEFT OUTER JOIN name n
-    ON n.id = ci.person_id
-  LEFT OUTER JOIN aka_name an
-    ON an.person_id = n.id
-  LEFT OUTER JOIN person_info pi
-    ON pi.person_id = n.id
-  LEFT OUTER JOIN info_type pit
-    ON pit.id = pi.info_type_id
-  LEFT OUTER JOIN movie_companies mcomp
-    ON mcomp.movie_id = t.id
-  LEFT OUTER JOIN company_name cn
-    ON cn.id = mcomp.company_id
-  LEFT OUTER JOIN company_type ct
-    ON ct.id = mcomp.company_type_id
-WHERE t.production_year BETWEEN 1900 AND 2025
-  AND t.title LIKE '%Disney%'
-  -- Require a significantly richer set of related rows:
-  AND (SELECT COUNT(*) FROM movie_keyword mk2 WHERE mk2.movie_id = t.id) >= 20
-  AND (SELECT COUNT(*) FROM movie_info mi2 WHERE mi2.movie_id = t.id) >= 15
-  AND (SELECT COUNT(*) FROM cast_info ci2 WHERE ci2.movie_id = t.id) >= 10
-ORDER BY t.production_year;
+  LEFT OUTER JOIN kind_type kt ON kt.id = t.kind_id
+  LEFT OUTER JOIN aka_title at ON at.movie_id = t.id
+  LEFT OUTER JOIN movie_info mi ON mi.movie_id = t.id
+  LEFT OUTER JOIN info_type mit ON mit.id = mi.info_type_id
+  LEFT OUTER JOIN movie_info_idx mii ON mii.movie_id = t.id
+  LEFT OUTER JOIN info_type miit ON miit.id = mii.info_type_id
+  LEFT OUTER JOIN movie_keyword mk ON mk.movie_id = t.id
+  LEFT OUTER JOIN keyword k ON k.id = mk.keyword_id
+  LEFT OUTER JOIN movie_link ml ON ml.movie_id = t.id
+  LEFT OUTER JOIN link_type lt ON lt.id = ml.link_type_id
+  LEFT OUTER JOIN complete_cast cc ON cc.movie_id = t.id
+  LEFT OUTER JOIN cast_info ci ON ci.movie_id = t.id
+  LEFT OUTER JOIN role_type rt ON rt.id = ci.role_id
+  LEFT OUTER JOIN comp_cast_type cct ON cct.id = ci.person_role_id
+  LEFT OUTER JOIN name n ON n.id = ci.person_id
+  LEFT OUTER JOIN aka_name an ON an.person_id = n.id
+  LEFT OUTER JOIN person_info pi ON pi.person_id = n.id
+  LEFT OUTER JOIN info_type pit ON pit.id = pi.info_type_id
+  LEFT OUTER JOIN movie_companies mcomp ON mcomp.movie_id = t.id
+  LEFT OUTER JOIN company_name cn ON cn.id = mcomp.company_id
+  LEFT OUTER JOIN company_type ct ON ct.id = mcomp.company_type_id
+WHERE lower(t.title) LIKE '%the%'
+ORDER BY t.production_year
+LIMIT 10000;
