d13ec169f4d4ce3436a94cb3d08665548f6fbebc
Johan Solbakken <johan.hvaler@gmail.com>
2025-04-24 11:28:15 +0200

    Sub queryify q5

--- oohj-bench-queries/q5.sql
+++ oohj-bench-queries/q5.sql
@@ -1,23 +1,123 @@
-SELECT *
-FROM title t
-JOIN aka_title at ON at.movie_id = t.id AND at.id < 10
-JOIN movie_info mi ON mi.movie_id = t.id AND mi.id < 10
-JOIN movie_info_idx mii ON mii.movie_id = t.id AND mii.id < 1000
-JOIN movie_keyword mk ON mk.movie_id = t.id AND mk.id < 1000
-JOIN movie_link ml ON ml.movie_id = t.id AND ml.id < 1000
-JOIN movie_companies mc ON mc.movie_id = t.id AND mc.id < 1000
-JOIN complete_cast cc ON cc.movie_id = t.id AND cc.id < 1000
-JOIN cast_info ci ON ci.movie_id = t.id AND ci.id < 1000
-JOIN name n ON n.id = ci.person_id AND n.id < 1000
-JOIN aka_name an ON an.person_id = n.id AND an.id < 1000
-JOIN person_info pi ON pi.person_id = n.id AND pi.id < 1000
-JOIN char_name cn ON cn.id = ci.role_id AND cn.id < 1000
-JOIN role_type rt ON rt.id = ci.role_id AND rt.id < 1000
-JOIN comp_cast_type cct ON cct.id = cc.subject_id AND cct.id < 1000
-JOIN company_name cna ON cna.id = mc.company_id AND cna.id < 1000
-JOIN company_type cty ON cty.id = mc.company_type_id AND cty.id < 1000
-JOIN info_type it ON it.id = mi.info_type_id AND it.id < 1000
-JOIN keyword k ON k.id = mk.keyword_id AND k.id < 1000
-JOIN link_type lt ON lt.id = ml.link_type_id AND lt.id < 1000
-JOIN kind_type kt ON kt.id = t.kind_id AND kt.id < 1000
+SELECT  *
+FROM    title AS t
+LEFT JOIN (
+    SELECT *
+    FROM   aka_title
+    WHERE  id < 10
+)          AS at  ON at.movie_id  = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   movie_info
+    WHERE  id < 10
+)          AS mi  ON mi.movie_id  = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   movie_info_idx
+    WHERE  id < 10
+)          AS mii ON mii.movie_id = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   movie_keyword
+    WHERE  id < 10
+)          AS mk  ON mk.movie_id  = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   movie_link
+    WHERE  id < 10
+)          AS ml  ON ml.movie_id  = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   movie_companies
+    WHERE  id < 10
+)          AS mc  ON mc.movie_id  = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   complete_cast
+    WHERE  id < 10
+)          AS cc  ON cc.movie_id  = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   cast_info
+    WHERE  id < 10
+)          AS ci  ON ci.movie_id  = t.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   name
+    WHERE  id < 10
+)          AS n   ON n.id        = ci.person_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   aka_name
+    WHERE  id < 10
+)          AS an  ON an.person_id = n.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   person_info
+    WHERE  id < 10
+)          AS pi  ON pi.person_id = n.id
+
+LEFT JOIN (
+    SELECT *
+    FROM   char_name
+    WHERE  id < 10
+)          AS cn  ON cn.id       = ci.role_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   role_type
+    WHERE  id < 10
+)          AS rt  ON rt.id       = ci.role_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   comp_cast_type
+    WHERE  id < 10
+)          AS cct ON cct.id      = cc.subject_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   company_name
+    WHERE  id < 10
+)          AS cna ON cna.id      = mc.company_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   company_type
+    WHERE  id < 10
+)          AS cty ON cty.id      = mc.company_type_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   info_type
+    WHERE  id < 10
+)          AS it  ON it.id       = mi.info_type_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   keyword
+    WHERE  id < 10
+)          AS k   ON k.id        = mk.keyword_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   link_type
+    WHERE  id < 10
+)          AS lt  ON lt.id       = ml.link_type_id
+
+LEFT JOIN (
+    SELECT *
+    FROM   kind_type
+    WHERE  id < 10
+)          AS kt  ON kt.id       = t.kind_id
+
 ORDER BY t.production_year;
