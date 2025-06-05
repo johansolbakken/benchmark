286f288d47517fe4714e811fa2a6b2a2167f6b1b
Johan Solbakken <johan.hvaler@gmail.com>
2025-06-05 17:53:59 +0200

    tune the rest

--- stress-test-queries/stq3.sql
+++ stress-test-queries/stq3.sql
@@ -8,7 +8,7 @@ FROM
         FROM
             aka_title
         WHERE
-            id < 5000
+            id < 2500
     ) AS at ON at.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -16,7 +16,7 @@ FROM
         FROM
             movie_info
         WHERE
-            id < 8000
+            id < 4000
     ) AS mi ON mi.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -24,7 +24,7 @@ FROM
         FROM
             movie_info_idx
         WHERE
-            id < 100000
+            id < 200000
     ) AS mii ON mii.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -32,7 +32,7 @@ FROM
         FROM
             movie_keyword
         WHERE
-            id < 20000
+            id < 40000
     ) AS mk ON mk.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -40,7 +40,7 @@ FROM
         FROM
             movie_link
         WHERE
-            id < 20000000
+            id < 10000000
     ) AS ml ON ml.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -48,7 +48,7 @@ FROM
         FROM
             movie_companies
         WHERE
-            id < 10000
+            id < 50000
     ) AS mc ON mc.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -56,7 +56,7 @@ FROM
         FROM
             complete_cast
         WHERE
-            id < 20000
+            id < 50000
     ) AS cc ON cc.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -64,7 +64,7 @@ FROM
         FROM
             cast_info
         WHERE
-            id < 30000
+            id < 100000
     ) AS ci ON ci.movie_id = t.id
     LEFT JOIN (
         SELECT
@@ -72,7 +72,7 @@ FROM
         FROM
             name
         WHERE
-            id < 10000
+            id < 1000
     ) AS n ON n.id = ci.person_id
     LEFT JOIN (
         SELECT
@@ -96,7 +96,7 @@ FROM
         FROM
             char_name
         WHERE
-            id < 4000
+            id < 1000
     ) AS cn ON cn.id = ci.role_id
     LEFT JOIN (
         SELECT
@@ -104,7 +104,7 @@ FROM
         FROM
             company_name
         WHERE
-            id < 4000
+            id < 5000
     ) AS cna ON cna.id = mc.company_id
     LEFT JOIN (
         SELECT
@@ -112,9 +112,9 @@ FROM
         FROM
             keyword
         WHERE
-            id < 4000
+            id < 1000
     ) AS k ON k.id = mk.keyword_id
 WHERE
-    t.id < 5000
+    t.id < 8000
 ORDER BY
     t.id;
